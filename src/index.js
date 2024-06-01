876017a91f7c70a9333e8b51668dbf9198dc6ece
derara <derarageremu@gmail.com>
2024-06-01 15:37:29 +0300

    fetching data

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
-console.log('index.js')
-console.log('version2')
+import http from 'http'
+
+const app = http.createServer((req,res) =>{
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS'); // Allow specific methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.end(JSON.stringify('hello'));
+})
+
+app.listen(4000,() => console.log('app running on port 4000'))
---
+++ src/main.js
@@ -0,0 +1,14 @@
+async function getData(){
+ try {
+    let res = await fetch("http://localhost:4000/")
+    let data = res.json()
+    return data
+ } catch (error) {
+    console.error('Error fetching data', error)
+ }
+}
+
+document.getElementById('button').addEventListener('click',async() =>{
+    const data = await getData()
+    document.getElementById('h').innerHTML = data
+})
